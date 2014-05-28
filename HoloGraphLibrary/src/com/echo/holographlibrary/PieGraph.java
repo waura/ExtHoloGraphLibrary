2b3a4a9d0b9e0ba8b779d377f2220917464378e5
Diogo Gomes <dgomes@ua.pt>
2014-05-28 17:29:54 +0100

    Case we click somewhere else, also get feedback!

--- HoloGraphLibrary/src/com/echo/holographlibrary/PieGraph.java
+++ HoloGraphLibrary/src/com/echo/holographlibrary/PieGraph.java
@@ -156,6 +156,12 @@ public class PieGraph extends View {
                 count++;
             }
         }
+        // Case we click somewhere else, also get feedback!
+        if(MotionEvent.ACTION_UP == event.getAction()
+                && mSelectedIndex == -1
+                && mListener != null) {
+            mListener.onClick(mSelectedIndex);
+        }
         // Reset selection
         if (MotionEvent.ACTION_UP == event.getAction()
                 || MotionEvent.ACTION_CANCEL == event.getAction()) {
